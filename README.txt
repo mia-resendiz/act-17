ef0b08d924280572d1c67446271a49f31fa6fe6c
mia-resendiz <160759884+mia-resendiz@users.noreply.github.com>
2024-06-14 02:43:54 +0000

    Initial commit

---
+++ README.txt
@@ -0,0 +1,8 @@
+inicio 
+mostrar: nombre;
+mostrar: calle;
+mostrar: colonia;
+mostrar: n* de casa;
+mostrar: municipio;
+mostrar: n* de telefono
+fin 
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Mia Stephania Resendiz Garcia</p>';
+     echo '<p>Colonia: zecista  </p>';
+    echo '<p>Calle:  pimpom  </p>';
+    echo '<p>No de Casa:  2809   </p>';
+    echo '<p>Muicipio:  guatemala </p>';
+    echo '<p>No de telefono: 844 590 3707  </p>';
+    ?> 
+</html>
